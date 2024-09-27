ecdd4445d8e1954b42128ef2b7fdc340e45df4e0
Гудкова Мария <92262191+acentuy@users.noreply.github.com>
2024-09-27 14:38:22 +0300

    refactor physics beams

--- assets/scripts/UnstableBeams.ts
+++ assets/scripts/UnstableBeams.ts
@@ -1,9 +1,9 @@
-import { _decorator, Component, RigidBody, Collider, ITriggerEvent, Camera } from 'cc';
+import { _decorator, Component, RigidBody, Collider, ITriggerEvent, Camera, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('UnstableBeams')
 export class UnstableBeams extends Component {
-    @property private mass: number = 1;
+    @property(Vec3) private initialForce: Vec3 = new Vec3(0, -100, 0);
     private _rigidBody: RigidBody = null;
     private _collider: Collider = null;
     private _mainCamera: Camera | null = null;
@@ -18,7 +18,7 @@ export class UnstableBeams extends Component {
         this._collider.off('onTriggerEnter', this._onTriggerEnter, this);
         this._collider.isTrigger = false;
         this._rigidBody.type = RigidBody.Type.DYNAMIC;
-        this._rigidBody.mass = this.mass;
+        this._rigidBody.applyForce(this.initialForce);
 
         this.scheduleOnce(this._destroyNode, 8);
     }
